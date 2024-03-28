660a1719b36c6b8201c665ceea6995b345492368
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-03-28 16:34:05 -0700

    tasksss

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,11 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+
+    header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
+    'referer':'https://www.google.com/'}
+
+    r = requests.get("https://intranet.hbtn.io/status",headers=header)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
