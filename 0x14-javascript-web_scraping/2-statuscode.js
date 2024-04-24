c658dc7ec1293b9902b8f0389d240f0b3d9c5050
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-24 15:37:45 -0700

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+// Import the 'request' module.
+
+// Use the 'request' module to perform an HTTP GET request to the URL.
+request.get(process.argv[2]).on('response', function (response) {
+    // Set up an event listener for the 'response' event emitted by the HTTP request.
+
+    console.log(`code: ${response.statusCode}`);
+    // Log the HTTP status code of the response to the console.
+});
